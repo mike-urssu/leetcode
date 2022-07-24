ff9c280b1b9fc9624392389634c6186574a09333
Joon Hee Song <mike.urssu@gmail.com>
2022-07-24 18:13:18 +0900

    PalindromeNumber: TestCode 수정

--- src/test/kotlin/easy/PalindromeNumberTest.kt
+++ src/test/kotlin/easy/PalindromeNumberTest.kt
@@ -2,12 +2,11 @@ package easy
 
 fun main() {
     val palindromeNumber = PalindromeNumber()
-    println(palindromeNumber.isPalindrome(121))
-    println(palindromeNumber.isPalindrome(123))
-    println(palindromeNumber.isPalindrome(-121))
-    println(palindromeNumber.isPalindrome(10))
-    println(palindromeNumber.isPalindrome(0))
-    println(palindromeNumber.isPalindrome(7))
-    println(palindromeNumber.isPalindrome(1410110141))
+    assert(palindromeNumber.isPalindrome(121))
+    assert(!palindromeNumber.isPalindrome(123))
+    assert(!palindromeNumber.isPalindrome(-121))
+    assert(!palindromeNumber.isPalindrome(10))
+    assert(palindromeNumber.isPalindrome(0))
+    assert(palindromeNumber.isPalindrome(7))
+    assert(palindromeNumber.isPalindrome(1410110141))
 }
-
