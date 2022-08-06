53ac202633b8dd3107029da2874d418886b5835f
Joon Hee Song <mike.urssu@gmail.com>
2022-08-06 19:16:32 +0900

    RemoveElement 재구현

--- src/main/kotlin/easy/RemoveElement.kt
+++ src/main/kotlin/easy/RemoveElement.kt
@@ -2,25 +2,13 @@ package easy
 
 class RemoveElement {
     fun removeElement(nums: IntArray, `val`: Int): Int {
-        var leftIndex = 0
-        var rightIndex = nums.size - 1
+        var index = 0
 
-        while (leftIndex < rightIndex) {
-            if (nums[leftIndex] == `val`) {
-                while (nums[rightIndex] == `val` && leftIndex < rightIndex)
-                    rightIndex--
-                swap(nums, leftIndex, rightIndex--)
-            }
-            leftIndex++
+        for (i in nums.indices) {
+            if (nums[i] != `val`)
+                nums[index++] = nums[i]
         }
 
-        val index = nums.indexOf(`val`)
-        return if (index == -1) nums.size else index
-    }
-
-    private fun swap(nums: IntArray, leftIndex: Int, rightIndex: Int) {
-        val temp = nums[leftIndex]
-        nums[leftIndex] = nums[rightIndex]
-        nums[rightIndex] = temp
+        return index
     }
 }
