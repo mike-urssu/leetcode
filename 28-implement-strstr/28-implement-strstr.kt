c5c8f25fbf463c3c2f347be61414ad245f997540
Joon Hee Song <mike.urssu@gmail.com>
2022-08-14 13:38:58 +0900

    Time: 185 ms (81.69%), Space: 34.5 MB (53.76%) - LeetHub

--- 28-implement-strstr/28-implement-strstr.kt
+++ 28-implement-strstr/28-implement-strstr.kt
@@ -1,5 +1,21 @@
 class Solution {
     fun strStr(haystack: String, needle: String): Int {
-        return haystack.indexOf(needle)
+        if (needle.length > haystack.length)
+            return -1
+
+        for (i in 0..haystack.length - needle.length) {
+            var isHaystackContainingNeedle = true
+            for (j in needle.indices) {
+                if (haystack[i + j] != needle[j]) {
+                    isHaystackContainingNeedle = false
+                    break
+                }
+            }
+            
+            if (isHaystackContainingNeedle)
+                return i
+        }
+
+        return -1
     }
 }
